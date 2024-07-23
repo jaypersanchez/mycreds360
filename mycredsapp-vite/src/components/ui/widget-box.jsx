e8bcccc6f7db354e3785207faf20577aa96444f2
jaypersanchez <jaypersanchez@gmail.com>
2024-07-24 07:01:37 +0800

    initial save

--- mycredsapp-vite/src/components/ui/widget-box.jsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export default function WidgetBox({ children, className, ...props }) {
-  return (
-    <div className={cn("shadow-lg rounded-xl", className)} {...props}>
-      {children}
-    </div>
-  );
-}
